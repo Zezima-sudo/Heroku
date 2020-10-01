321774395ea6d58dfe5e9835b71a60a78199ee5d
Eric Rice <ericmichaelrice@protonmail.com>
2020-10-01 14:05:31 -0400

    added env

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express ()
 server.get('/', (req, res) => {
     res.status(200).json({
         message: 'Hello node34',
-        environment: process.env.PORT
+        environment: process.env.NODE_ENV
     })
 })
 
